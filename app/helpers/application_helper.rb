e601269646204860b018e77058c4fdfafafc4b2e
denvermullets <devmullets@gmail.com>
2025-03-09 08:27:25 -0400

    feat(progress): add progress bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,21 @@ module ApplicationHelper
   def strip_abbr(name)
     name.gsub(/\[.*?\]/, '')
   end
+
+  # rubocop:disable Lint/DuplicateBranch
+  def determine_bill_status(bill)
+    status = bill.latest_actions.order(:action_date).last.text.downcase
+
+    if status.include?('committee')
+      'passed_house'
+    elsif status.include?('received in the senate')
+      # i think??
+      'passed_senate'
+    elsif status.include?('submitted in the senate')
+      'passed_house'
+    else
+      'passed_house'
+    end
+  end
+  # rubocop:enable Lint/DuplicateBranch
 end
